2dcd42f599eb9e5c8022b3d8f7648490ad2028f0
hideokamoto <hide.okamoto@digitalcube.jp>
2018-09-19 21:35:56 +0900

    asku setup lint

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,10 +1,121 @@
 const {Command, flags} = require('@oclif/command')
+const {execFile} = require('child_process')
+const fs = require('fs')
+
+const npmInstall = (commands, self) => new Promise((resolve, reject) => {
+  execFile('npm', commands, (error, stdout, stderr) => {
+    self.log(stderr)
+    if (error) {
+      self.log(error)
+      return reject(error)
+    }
+    self.log(stdout)
+    return resolve(true)
+  })
+})
+const createNpmInstallCommands = (packages, type = 'dev') => {
+  switch (type) {
+  case 'prod':
+  case 'production': {
+    const prodCommands = ['i', '-S']
+    Array.prototype.push.apply(prodCommands, packages)
+    return prodCommands
+  }
+  default: {
+    const devCommands = ['i', '-D']
+    Array.prototype.push.apply(devCommands, packages)
+    return devCommands
+  }
+  }
+}
+const defaultPath = './lambda/custom'
 
 class SetupCommand extends Command {
+  async createSamTemplate() {
+
+  }
+
+  async createServerlessYaml() {
+
+  }
+
+  async installLintPackages() {
+    this.log('Installing lint packages by npm')
+    const packages = ['eslint']
+    const commands = createNpmInstallCommands(packages, 'dev')
+    try {
+      process.chdir(this.path)
+    } catch (e) {
+      this.log(e)
+      this.exit(1)
+    }
+    try {
+      await npmInstall(commands, this)
+    } catch (e) {
+      this.log(e)
+    }
+    this.log('Installed lint packages.')
+  }
+
+  async updatePackageJsonForLint() {
+    let content
+    const path = `${this.path}/package.json`
+    try {
+      content = fs.readFileSync(path, 'utf8')
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        this.log(e)
+        this.exit(1)
+        return
+      }
+    }
+    const pkg = JSON.parse(content)
+    if (pkg.scripts.lint) {
+      this.log('[Skip] lint command is already defined.')
+    }
+    pkg.scripts.lint = './node_modules/.bin/eslint *.js'
+    try {
+      fs.writeFileSync(path, JSON.stringify(pkg, null, '\t'))
+    } catch (e) {
+      this.log(e)
+      this.exit(1)
+    }
+  }
+
+  async createEslintIgnore() {
+    this.log('Add ESLint ignore file.')
+    let content = ''
+    const path = `${this.path}/.eslintignore`
+    try {
+      content = fs.readFileSync(path, 'utf8')
+      if (/webpack.config.js/g.test(content)) return
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        this.log(e)
+        this.exit(1)
+        return
+      }
+    }
+    content += '\nwebpack.config.js'
+    try {
+      fs.writeFileSync(path, content)
+    } catch (e) {
+      this.log(e)
+      this.exit(1)
+    }
+    this.log('ESLint ignore file created.')
+  }
+
   async run() {
-    const {flags} = this.parse(SetupCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/dc_hideokamoto/develop/cli/ask-utils-cli/src/commands/setup.js`)
+    const {args, flags} = this.parse(SetupCommand)
+    this.path = flags.path || defaultPath
+    switch (args.type) {
+    case 'lint': {
+      await this.installLintPackages()
+      await this.updatePackageJsonForLint()
+      await this.createEslintIgnore()
+    }
+    }
   }
 }
 
@@ -14,7 +125,8 @@ Extra documentation goes here
 `
 
 SetupCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  help: flags.help({char: 'h'}),
+  path: flags.string({char: 'p', description: 'working dirctory path'}),
 }
 SetupCommand.args = [
   {
